1d383e11df8593629b765b54049cf9c813c6c97d
Kunto Aji. K <kuntoaji@kaklabs.com>
2018-10-28 10:32:11 +0700

    install and configure enkrip

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,11 @@
 class Post < ApplicationRecord
+  include Enkrip::Model
+
+  enkrip_configure do |config|
+    config.string_attributes << :my_string
+    config.numeric_attributes << :my_numeric
+  end
+
+  validates :my_numeric, numericality: { greater_than: 0 }
+  validates :my_string, presence: true
 end
